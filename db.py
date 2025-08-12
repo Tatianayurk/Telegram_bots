eb703a43bfff6c2d9ec61eaac0fecb661731a789
Envelope <nikolajrodin905@gmail.com>
2025-08-12 20:49:15 +0400

    change connect type

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 import mysql.connector
+import database_info
 
 db = mysql.connector.connect(
-    host='127.0.0.1',
-    port='3306',#Который указывали при установке
-    user='root',
-    password='root',#Пароль который указывали при установке
-    database = 'telegram_bot' #Название которое сделали в sql workbench
+    host=database_info.host,
+    port=database_info.port,#Который указывали при установке
+    user=database_info.user,
+    password=database_info.password,#Пароль который указывали при установке
+    database = database_info.database #Название которое сделали в sql workbench
 )
 
 db.autocommit = True #Свойство автоотправки запросов insert
